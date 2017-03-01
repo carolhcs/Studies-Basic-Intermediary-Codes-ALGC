2658f7f8e6b8dbf580b101e4d284740017feb3f8
carolhcs <carol_lgl@hotmail.com>
2017-02-28 23:31:21 -0300

    aula0

---
+++ Java/JavaAulasIniciante01/src/javaaulasiniciante01/JavaAulasIniciante01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaaulasiniciante01;
+
+/**
+ *
+ * @author carolhcs
+ */
+public class JavaAulasIniciante01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean flag = 5 > 2;
+        
+        if (flag){
+            System.out.println("Flag Verdadeira");
+        } else {
+            System.out.println("Flag Falsa");
+        }
+        
+        System.out.println(5.0/2);
+        System.out.println(3.0/5.0*2+3-0.5);
+    }
+    
+}
