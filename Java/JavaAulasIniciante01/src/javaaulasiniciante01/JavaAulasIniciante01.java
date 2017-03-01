42291a44b74359c297dbd4b42717339099eae21f
carolhcs <carol_lgl@hotmail.com>
2017-03-01 12:59:05 -0300

    att

--- Java/JavaAulasIniciante01/src/javaaulasiniciante01/JavaAulasIniciante01.java
+++ Java/JavaAulasIniciante01/src/javaaulasiniciante01/JavaAulasIniciante01.java
@@ -16,16 +16,51 @@ public class JavaAulasIniciante01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        boolean flag = 5 > 2;
+        
+        // Aula 01
+        /*boolean flag = 5 > 2;
         
         if (flag){
             System.out.println("Flag Verdadeira");
         } else {
             System.out.println("Flag Falsa");
         }
-        
         System.out.println(5.0/2);
-        System.out.println(3.0/5.0*2+3-0.5);
+        System.out.println(3.0/5.0*2+3-0.5);*/
+        
+        //Aula 02 ATM
+        /*int originalAmount = 1245;
+        int amount = originalAmount;
+        int n100 = amount / 100;
+        amount %= 100;
+        int n50 = amount / 50;
+        amount %= 50;
+        int n20 = amount / 20;
+        amount %= 20;
+        int n10 = amount / 10;
+        amount %= 10;
+        int n5 = amount / 5;
+        amount %= 5;
+        int n2 = amount / 2;
+        amount %= 2;
+        System.out.println("Notas de R$ 100,00: " +n100);
+        System.out.println("Restos: " +amount);*/
+        
+        //Aula 03 ATM
+        int originalAmount = 2356;
+        int amount = originalAmount;
+
+        System.out.println("Dividindo R$ " + originalAmount + " em notas de R$ 100, " + " R$ 50,  R$ 20,  R$ 10,  R$ 5,  R$ 2,  R$ 1, ");
+
+        int[] billValues = {100, 50, 20, 10, 5, 2, 1};
+        int[] numberOfBills = new int[billValues.length];
+
+        for (int i=0; i < billValues.length; i++){
+            //System.out.println(billValues[i]);
+            numberOfBills[i] = amount / billValues[i];
+            amount %= billValues[i];
+            System.out.println("Notas de R$ " + billValues[i] + ": " + numberOfBills[i]);
+        }
     }
     
 }
